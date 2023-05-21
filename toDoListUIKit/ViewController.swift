9460dea6dc008a4f2e9ada00a313a2b8508af010
boztopuz <boztopuz@gmail.com>
2023-05-21 14:46:51 +0300

    Initial Commit

---
+++ toDoListUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoListUIKit
+//
+//  Created by Burak Öztopuz on 21.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
